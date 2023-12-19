996626de9150f821cd6d3817cba37dcddd306ac9
RafiMeuraxa <149429153+RafiMeuraxa@users.noreply.github.com>
2023-12-19 13:23:35 +0700

    Delete Matematika directory

--- Matematika/script.js
+++
@@ -1,4 +0,0 @@
-function toggleAnswer(questionNumber) {
-    var answer = document.getElementById("answer" + questionNumber);
-    answer.style.display = (answer.style.display === "none" || answer.style.display === "") ? "block" : "none";
-}
